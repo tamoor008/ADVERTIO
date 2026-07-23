d86854830548179acba520b3f1f35310aaf75169
Tamoor Malik <tamoormalik088@gmail.com>
2026-07-24 00:55:33 +0500

    ok

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,16 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/about": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/about.js"
-    ],
-    "/contact": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/contact.js"
     ]
   },
   "ampFirstPages": []
